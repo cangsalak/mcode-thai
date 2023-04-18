2cb86c9cb5fa5e77bdfc5dbb66ef1061c73de52e
cangsalak <max_kai@hotmail.co.th>
2023-04-18 13:37:49 +0700

    Update sosmed.php

--- application/modules/setting/views/sosmed.php
+++ application/modules/setting/views/sosmed.php
@@ -1,5 +1,10 @@
 <link rel="stylesheet" href="<?=base_url()?>_temp/backend/vendors/xeditable/bootstrap-editable.css">
 <script src="<?=base_url()?>_temp/backend/vendors/xeditable/bootstrap-editable.min.js"></script>
+<style media="screen">
+  a:hover{
+    text-decoration: none;
+  }
+</style>
 <div class="row">
   <div class="col-md-12 col-xl-12 mx-auto animated fadeIn delay-2s grid-margin stretch-card">
     <div class="card">
@@ -9,35 +14,35 @@
         <?php if (is_allowed("config_view_sosmed")): ?>
         <table class="table-setting table-striped table-hover">
           <tr>
-            <td class="table-title"><i class="ti-facebook cl-facebook"></i> <?=cclang("Facebook")?></td>
+            <td class="table-title"><i class="ti-facebook cl-facebook"></i> <a href="<?=setting('facebook')?>" target="_blank"><?=cclang("Facebook")?></a> </td>
             <td>
               <a href="javascript:void(0);" id="facebook" data-url="<?=url("setting/update_action")?>" data-type="text" data-pk="50" class="editable editable-click" title="<?=cclang("update")?>"><?=setting('facebook')?></a>
             </td>
           </tr>
 
           <tr>
-            <td class="table-title"><i class="ti-instagram cl-instagram"></i> <?=cclang("Instagram")?></td>
+            <td class="table-title"><i class="ti-instagram cl-instagram"></i> <a href="<?=setting('instagram')?>" target="_blank"> <?=cclang("Instagram")?></a></td>
             <td>
               <a href="javascript:void(0);" id="instagram" data-url="<?=url("setting/update_action")?>" data-type="text" data-pk="51" class="editable editable-click" title="<?=cclang("update")?>"><?=setting('instagram')?></a>
             </td>
           </tr>
 
           <tr>
-            <td class="table-title"><i class="ti-youtube cl-youtube"></i> <?=cclang("Youtube")?></td>
+            <td class="table-title"><i class="ti-youtube cl-youtube"></i><a href="<?=setting('youtube')?>" target="_blank"> <?=cclang("Youtube")?> </a></td>
             <td>
               <a href="javascript:void(0);" id="youtube" data-url="<?=url("setting/update_action")?>" data-type="text" data-pk="52" class="editable editable-click" title="<?=cclang("update")?>"><?=setting('youtube')?></a>
             </td>
           </tr>
 
           <tr>
-            <td class="table-title"><i class="ti-twitter cl-twitter"></i> <?=cclang("Twitter")?></td>
+            <td class="table-title"><i class="ti-twitter cl-twitter"></i> <a href="<?=setting('twitter')?>" target="_blank"> <?=cclang("Twitter")?></a></td>
             <td>
               <a href="javascript:void(0);" id="twitter" data-url="<?=url("setting/update_action")?>" data-type="text" data-pk="53" class="editable editable-click" title="<?=cclang("update")?>"><?=setting('twitter')?></a>
             </td>
           </tr>
 
           <tr>
-            <td class="table-title"><i class="ti-layers cl-layers"></i> <?=cclang("Web Site")?></td>
+            <td class="table-title"><i class="ti-layers cl-layers"></i> <a href="<?=setting('website')?>" target="_blank"> <?=cclang("Web Site")?></a></td>
             <td>
               <a href="javascript:void(0);" id="website" data-url="<?=url("setting/update_action")?>" data-type="text" data-pk="54" class="editable editable-click" title="<?=cclang("update")?>"><?=setting('website')?></a>
             </td>
